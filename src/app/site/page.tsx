7dfa507e9bde4748b10bd00d285d7cc68e883e06
mohits-git <mohitkhatri2805@gmail.com>
2024-03-19 22:55:19 +0530

    fix: fix navigation bar to have fixed position at top

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <section className="h-full w-full md:pt-44 relative flex flex-col items-center justify-center">
+      <section className="h-full w-full mt-4 md:pt-44 relative flex flex-col items-center justify-center">
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]
         dark:bg-[linear-gradient(to_right,#666666_1px,transparent_1px),linear-gradient(to_bottom,#666666_1px,transparent_1px)]" />
         <p className="text-center">Run your agency in one place.</p>
