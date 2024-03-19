9685909ee0086355f79bbab859b826fb991b3786
mohits-git <mohitkhatri2805@gmail.com>
2024-03-19 22:43:16 +0530

    fix: landing page grid style fix for dark mode

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -9,7 +9,8 @@ export default function Home() {
   return (
     <>
       <section className="h-full w-full md:pt-44 relative flex flex-col items-center justify-center">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]
+        dark:bg-[linear-gradient(to_right,#666666_1px,transparent_1px),linear-gradient(to_bottom,#666666_1px,transparent_1px)]" />
         <p className="text-center">Run your agency in one place.</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[300px]">Plura</h1>
