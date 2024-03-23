9b28e4c85eb1b6439ea4045c1eae83914fa41c79
mohits-git <mohitkhatri2805@gmail.com>
2024-03-23 22:40:14 +0530

    rename: AgencyDetails.tsx -> agency-details.tsx

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,4 +1,4 @@
-import AgencyDetails from "@/components/forms/AgencyDetails";
+import AgencyDetails from "@/components/forms/agency-details";
 import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
 import { Plan } from "@prisma/client";
