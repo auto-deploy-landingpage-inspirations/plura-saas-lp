4f73c9ca15996597a4f2a80bfd04c0ada53d47ac
mohits-git <mohitkhatri2805@gmail.com>
2024-03-27 13:46:11 +0530

    feat: added sendInvitation query

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -488,3 +488,28 @@ export const getUser = async (id: string) => {
   return user
 }
 
+export const sendInvitation = async (
+  role: Role,
+  email: string,
+  agencyId: string
+) => {
+  const response = await db.invitation.create({
+    data: { email, agencyId, role },
+  })
+
+  try {
+    const invitation = await clerkClient.invitations.createInvitation({
+      emailAddress: email,
+      redirectUrl: process.env.NEXT_PUBLIC_URL,
+      publicMetadata: {
+        throughInvitation: true,
+        role,
+      },
+    });
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+
+  return response;
+}
