4950c43ba27aa9b21cad9f14835bcb1c4f262c6c
mohits-git <mohitkhatri2805@gmail.com>
2024-03-30 23:18:34 +0530

    feat: added subaccountId subpage

---
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-settings.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+
+}
+
+const PipelineSettings: React.FC<Props> = () => {
+    return (
+        <>
+            PipelineSettings
+        </>
+    )
+}
+
+export default PipelineSettings
