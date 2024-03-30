52281e3cc8d39bb6060f6d15a84b1ddeea695041
mohits-git <mohitkhatri2805@gmail.com>
2024-03-30 23:14:36 +0530

    feat: added TicketAndTags, LaneDetails, and Create funnel and pipelines schema

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,7 @@
-import { Notification, Prisma, Role } from "@prisma/client";
+import { Contact, Lane, Notification, Prisma, Role, Tag, Ticket, User } from "@prisma/client";
 import { getAuthUserDetails, getMedia, getUserPermissions } from "./queries";
 import { db } from "./db";
+import { z } from "zod";
 
 export type NotificationWithUser =
   | ({
@@ -15,7 +16,7 @@ export type NotificationWithUser =
       agencyId: string | null
     }
   } & Notification)[]
-  | undefined 
+  | undefined
 
 export type UserWithPermissionsAndSubAccounts = Prisma.PromiseReturnType<
   typeof getUserPermissions
@@ -43,3 +44,24 @@ export type UsersWithAgencySubAccountPermissionsSidebarOptions =
 export type GetMediaFiles = Prisma.PromiseReturnType<typeof getMedia>
 
 export type CreateMediaType = Prisma.MediaCreateWithoutSubaccountInput;
+
+export type TicketAndTags = Ticket & {
+  Tags: Tag[];
+  Assigned: User | null;
+  Customer: Contact | null;
+}
+
+export type LaneDetail = Lane & {
+  Tickets: TicketAndTags[]
+}
+
+export const CreatePipelineFormSchema = z.object({
+  name: z.string().min(1),
+})
+
+export const CreateFunnelFormSchema = z.object({
+  name: z.string().min(1),
+  description: z.string(),
+  subDomainName: z.string().optional(),
+  favicon: z.string().optional(),
+})
