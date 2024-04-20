523627a641d849057867fe29b593dcf623bda797
mohits-git <mohitkhatri2805@gmail.com>
2024-04-20 22:10:52 +0530

    feat: added funnel editor navigation bar

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
@@ -1,6 +1,9 @@
 import { db } from "@/lib/db";
+import EditorProvider from "@/providers/editor/editor-provider";
 import { redirect } from "next/navigation";
 import React from "react";
+import FunnelEditorNavigation from "./_components/funnel-editor-navigation";
+import FunnelEditorSidebar from "./_components/funnel-editor-sidebar";
 
 type Props = {
   params: {
@@ -19,9 +22,21 @@ const Page: React.FC<Props> = async ({ params }) => {
   }
 
   return (
-  <div className="fixed top-0 bottom-0 left-0 right-0 z-[20] overflow-hidden bg-background">
-    
-  </div>
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-[20] bg-background overflow-hidden">
+      <EditorProvider
+        subaccountId={params.subaccountId}
+        funnelId={params.funnelId}
+        pageDetails={funnelPageDetails}
+      >
+        <FunnelEditorNavigation 
+          funnelId={params.funnelId}
+          funnelPageDetails={funnelPageDetails}
+          subaccountId={params.subaccountId}
+        />
+        <FunnelEditorSidebar
+        />
+      </EditorProvider>
+    </div>
   )
 }
 
