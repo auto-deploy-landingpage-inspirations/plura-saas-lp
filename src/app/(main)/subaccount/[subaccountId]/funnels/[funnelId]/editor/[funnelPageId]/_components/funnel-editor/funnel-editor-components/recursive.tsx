d395888132db4509c843ea2a8ba1542843b14be2
mohits-git <mohitkhatri2805@gmail.com>
2024-04-21 19:38:05 +0530

    feat: added funnel-editor, with funnel-editor text, container and Recursive component

---
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/recursive.tsx
@@ -0,0 +1,22 @@
+import { EditorElement } from "@/providers/editor/editor-provider";
+import React from "react";
+import TextComponent from "./text";
+import Container from "./container";
+
+type Props = {
+  element: EditorElement;
+}
+
+const Recursive: React.FC<Props> = ({ element }) => {
+  switch (element.type) {
+    case "text": 
+      return <TextComponent element={element} />;
+    case "__body":
+      return <Container element={element} />;
+    default: 
+      return null;
+      
+  }
+}
+
+export default Recursive
